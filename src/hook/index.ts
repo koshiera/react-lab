5ffcf007d632870878e4866b41a9993c6b8fab3f
Sergey Kvint <koshiera@gmail.com>
2021-04-25 16:04:43 +0400

    Render count tool

--- src/hook/index.ts
+++ src/hook/index.ts
@@ -1,2 +1,3 @@
 export * from './useUser';
 export * from './useTag';
+export * from './useRenderCount';
---
+++ src/service/Counter.ts
@@ -0,0 +1,28 @@
+export class Counter {
+
+	public data: { [name: string]: number } = {};
+
+	public count(label: string = "default"): void {
+		if (!(label in this.data)) {
+			this.clear(label);
+		}
+		this.data[label]++;
+		console.log(`${label}.renders: ${this.data[label]}`);
+	}
+
+	public get(label: string = "default"): number | undefined {
+		if (label in this.data) {
+			return this.data[label];
+		}
+		return undefined;
+	}
+
+	public clear(label: string = "default"): void {
+		this.data[label] = 0;
+	}
+
+	public clearAll(): void {
+		this.data = {};
+	}
+
+}
