bb5ebd518595f9311cd7305057124e369da589fe
Sergey Kvint <koshiera@gmail.com>
2021-04-29 14:25:18 +0400

    Store (to save draft)

--- src/service/Store.tsx
+++ src/service/Store.tsx
@@ -2,26 +2,27 @@ import React, { FC, Dispatch, createContext, useContext, useReducer, useMemo } f
 
 import * as reducers from '../reducers';
 
-const combineReducers = () => {
-
-}
-
-type ReducerType<T extends readonly [ any, any ]> = [
+type Reducer<T extends readonly [ any, any ]> = [
 	Parameters<T[0]>[0],
 	Dispatch<Parameters<T[0]>[1]>
 ]
 
 type Context = {
-	user: ReducerType<typeof reducers.user>,
-	tag: ReducerType<typeof reducers.tag>
-}
-
-const context: Context = {
-	user: [ reducers.user[1], () => {} ],
-	tag: [ reducers.tag[1], () => {} ],
+	// [key in keyof typeof reducers]: typeof reducers[key]
+	[key in keyof typeof reducers]: Reducer<typeof reducers[key]>
 };
 
-const Context = createContext<Context>(context);
+// type Context = {
+// 	user: ReducerType<typeof reducers.user>,
+// 	tag: ReducerType<typeof reducers.tag>
+// }
+
+// const context: Context = {
+// 	user: [ reducers.user[1], () => {} ],
+// 	tag: [ reducers.tag[1], () => {} ],
+// };
+
+const Context = createContext({} as Context);
 
 const Provider: FC = ({ children }) => {
 
